eb33f2ea1bb69658d1c123139a0d63cdad062229
윤승현 <hyeondc99@gmail.com>
2023-09-23 21:54:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.33 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map= new HashMap<>();
+        for(String s: participant){
+            map.put(s,map.getOrDefault(s,0)+1);
+        }
+        for(String s: completion){
+            map.put(s,map.get(s)-1);
+        }
+        for(String s: map.keySet()){
+            if(map.get(s)!=0) answer=s;
+        }
+        return answer;
+    }
+}
