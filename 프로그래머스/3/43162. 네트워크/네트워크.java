a88c113fe1b61423ca9533756359581a4293223e
윤승현 <hyeondc99@gmail.com>
2023-10-21 02:39:32 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,22 +1,33 @@
 class Solution {
-    public boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        visited= new boolean[n];
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                // 방문하지 않은 컴퓨터부터 DFS 탐색 시작
                 dfs(computers, visited, i);
                 answer++;
             }
         }
+
         return answer;
     }
-    public void dfs(int[][] computers, boolean[] visited, int start){
-        visited[start]=true;
-        for(int i=0; i<computers[0].length; i++){
-            if(computers[start][i]==1 && !visited[i]){
+
+    private void dfs(int[][] computers, boolean[] visited, int start) {
+        visited[start] = true;
+
+        for (int i = 0; i < computers[start].length; i++) {
+            if (computers[start][i] == 1 && !visited[i]) {
+                // 연결된 컴퓨터이고 아직 방문하지 않았다면 재귀적으로 DFS 호출
                 dfs(computers, visited, i);
             }
         }
     }
 }
+
+
+
+
+
+
